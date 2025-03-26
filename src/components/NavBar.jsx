8b123257284bcd9ec3c6fb7384fa2acc65a8f921
BJ <bjdedushaj@yahoo.com>
2025-03-26 13:42:12 -0500

    fixed nav links not being fully clickable. only text previously.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,27 +6,43 @@ const NavBar = () => {
   const path = usePathname();
   console.log(path);
 
+  const navItemDefault = "rounded-4xl px-6 py-2 font-bold hover:bg-[#b5118f3f]";
+  const navItemActive = "border-1 border-[#b5118f7f]";
+
   return (
     <header className="w-full bg-[#b5118f14] border-b-[#b5118f3f] border-b-1 p-4 md:p-8 box-border">
       <nav className="max-w-[1200px] m-auto flex flex-wrap sm:justify-between justify-center items-center gap-4">
         <h1 className="text-3xl font-bold">MLB Standings</h1>
-        <ul className="inline-flex gap-2  *:rounded-4xl *:px-6 *:py-2 *:font-bold *:hover:bg-[#b5118f3f]">
-          <li className={path == "/" ? "border-1 border-[#b5118f7f]" : ""}>
-            <Link href="/">Home</Link>
+        <ul className="inline-flex gap-2 mt-1 sm:mt-0 pb-2 sm:pb-0">
+          <li>
+            <Link
+              href="/"
+              className={`${navItemDefault} ${
+                path === "/" ? navItemActive : ""
+              }`}
+            >
+              Home
+            </Link>
           </li>
-          <li
-            className={
-              path == "/teams" || path.includes("team")
-                ? "border-1 border-[#b5118f7f]"
-                : ""
-            }
-          >
-            <Link href="/teams">Teams</Link>
+          <li>
+            <Link
+              href="/teams"
+              className={`${navItemDefault} ${
+                path.includes("team") ? navItemActive : ""
+              }`}
+            >
+              Teams
+            </Link>
           </li>
-          <li
-            className={path == "/scores" ? "border-1 border-[#b5118f7f]" : ""}
-          >
-            <Link href="/scores">Scores</Link>
+          <li>
+            <Link
+              href="/scores"
+              className={`${navItemDefault} ${
+                path === "/scores" ? navItemActive : ""
+              }`}
+            >
+              Scores
+            </Link>
           </li>
         </ul>
       </nav>
